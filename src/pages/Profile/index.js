ce489487bfb9bc44c91b1eb083d7eb3bcb891192
v1eira <ewersonv@gmail.com>
2019-10-27 04:01:23 -0300

    add estilizaçao das rotas

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function Profile() {
+  return <Background />;
+}
+
+Profile.navigationOptions = {
+  tabBarLabel: 'My profile',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="person" size={20} color={tintColor} />
+  ),
+};
